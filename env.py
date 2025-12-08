036a6c0077a27d24071b7495cbc9bb81751f8bcf
Kerem Haeger <keremhaeger@gmail.com>
2025-12-08 10:26:35 +0000

    Add PostgreSQL DB and secret key

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgresql://neondb_owner:npg_N56jtlTRAQuJ@ep-winter-union-a2itpeka.eu-central-1.aws.neon.tech/ranch_lair_swipe_3925"  # noqa: E501
+os.environ["SECRET_KEY"] = "%J}0>b93yNH5NkrD=ua+3P{E=9y,g&"
--- filmhive/settings.py
+++ filmhive/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y-u%+!m9g5_)0ady97c96@%u)xlfo7-2nq=yyc2$nn(!8+qaeq'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,10 +84,11 @@ WSGI_APPLICATION = 'filmhive.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
 
 
