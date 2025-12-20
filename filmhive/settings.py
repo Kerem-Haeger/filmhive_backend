1e6b883fe62f6c42f654dc9f8046cf7830086e3d
Kerem Haeger <keremhaeger@gmail.com>
2025-12-20 11:46:41 +0000

    Remove comment from settings.py

--- filmhive/settings.py
+++ filmhive/settings.py
@@ -17,9 +17,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
